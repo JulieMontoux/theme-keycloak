88ab82c34607159b7a1e380f1b0d15c89633690d
JulieMontoux <julie.montoux@epsi.fr>
2025-05-20 14:14:12 +0200

    fix

--- my-sandbox/src/keycloak-theme/login/Login.tsx
+++ my-sandbox/src/keycloak-theme/login/Login.tsx
@@ -83,7 +83,7 @@ export default function Login(props: KcProps) {
 
           <div className="kcFormGroupClass">
             <button type="submit" id="kc-login" className="kcButtonClass">
-              Connexion
+              Connexion :)
             </button>
           </div>
 
