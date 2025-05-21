6f5459f922002d451efe9a7252af566c4d1f7b4d
JulieMontoux <julie.montoux@epsi.fr>
2025-05-21 10:52:16 +0200

    feat : init index

---
+++ dino/src/index.tsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode, lazy, Suspense } from "react";
+import { KcPage, type KcContext } from "./keycloak-theme/kc.gen";
+const AppEntrypoint = lazy(() => import("./index.app"));
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {window.kcContext ? (
+      <KcPage kcContext={window.kcContext} />
+    ) : (
+      <Suspense>
+        <AppEntrypoint />
+      </Suspense>
+    )}
+  </StrictMode>
+);
+
+declare global {
+  interface Window {
+    kcContext?: KcContext;
+  }
+}
